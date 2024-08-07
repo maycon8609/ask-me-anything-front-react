293a4f27a2483f5592eb481e6202965dd9dd0898
maycon silva <maycon8609@gmail.com>
2024-08-07 17:43:38 -0300

    feat: added sonner

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,25 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
 
-import { CreateRoom } from './pages/create-room'
-import { Room } from './pages/room'
+import { CreateRoom } from "./pages/create-room";
+import { Room } from "./pages/room";
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <CreateRoom />
+    path: "/",
+    element: <CreateRoom />,
   },
   {
-    path: '/room/:roomId',
-    element: <Room />
-  }
-])
+    path: "/room/:roomId",
+    element: <Room />,
+  },
+]);
 
 export function App() {
-  return <RouterProvider router={router} />
+  return (
+    <>
+      <RouterProvider router={router} />
+      <Toaster invert richColors />
+    </>
+  );
 }
-
