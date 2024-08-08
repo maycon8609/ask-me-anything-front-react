3d6b19cc0999103019dfb9ccd7ac65b7b3ef933f
maycon silva <maycon8609@gmail.com>
2024-08-08 15:48:30 -0300

    feat: create room

--- src/pages/create-room.tsx
+++ src/pages/create-room.tsx
@@ -1,15 +1,25 @@
 import { ArrowRight } from "lucide-react";
+import { toast } from "sonner";
 
 import amaLogo from "../assets/ama-logo.svg";
 import { useNavigate } from "react-router-dom";
+import { createRoom } from "../http/create-room";
 
 export function CreateRoom() {
   const navigate = useNavigate();
 
-  function handleCreateRoom(data: FormData) {
-    const theme = data.get('theme')?.toString()
+  async function handleCreateRoom(data: FormData) {
+    const theme = data.get("theme")?.toString();
 
-    if (theme) navigate(`/room/${theme}`);
+    if (!theme) return;
+
+    try {
+      const { roomId } = await createRoom({ theme });
+
+      navigate(`/room/${roomId}`);
+    } catch {
+      toast.error("Erro ao criar a sala");
+    }
   }
 
   return (
@@ -31,6 +41,7 @@ export function CreateRoom() {
             name="theme"
             placeholder="Nome da sala"
             autoComplete="off"
+            required
             className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
           />
 
--- src/pages/room.tsx
+++ src/pages/room.tsx
@@ -1,9 +1,11 @@
+import { Suspense } from "react";
 import { useParams } from "react-router-dom";
-import { ArrowRight, Share2 } from "lucide-react";
+import { Share2 } from "lucide-react";
 import { toast } from "sonner";
 
-import { Message } from "../components/message";
 import amaLogo from "../assets/ama-logo.svg";
+import { Messages } from "../components/messages";
+import { CreateMessageForm } from "../components/create-message-form";
 
 export function Room() {
   const { roomId } = useParams();
@@ -16,7 +18,7 @@ export function Room() {
     } else {
       navigator.clipboard.writeText(url);
 
-      toast.info("The room URL was copied to your clipboard!");
+      toast.info("O link da sala foi copiado para área de transferência!");
     }
   }
 
@@ -41,52 +43,11 @@ export function Room() {
 
       <div className="h-px w-full bg-zinc-900" />
 
-      <form className="flex items-center gap-2 bg-zinc-900 p-2 rounded-xl border border-zinc-800 ring-orange-400 ring-offset-2 ring-offset-zinc-950 focus-within:ring-1">
-        <input
-          type="text"
-          name="question"
-          placeholder="Qual a sua pergunta?"
-          autoComplete="off"
-          className="flex-1 text-sm bg-transparent mx-2 outline-none text-zinc-100 placeholder:text-zinc-500"
-        />
+      <CreateMessageForm />
 
-        <button
-          type="submit"
-          className="bg-orange-400 text-orange-950 px-3 py-1.5 gap-1.5 flex items-center rounded-lg font-medium text-sm transition-colors hover:bg-orange-500"
-        >
-          Criar pergunt
-          <ArrowRight className="size-4" />
-        </button>
-      </form>
-
-      <ol className="list-decimal list-outside px-3 space-y-8">
-        <Message
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea quibusdam reiciendis ab ex voluptatum exercitationem cumque, dolores voluptate odio minima quod iure quam quis et eaque excepturi ipsam! Voluptates."
-          reactionsCount={2}
-          answered
-        />
-        <Message
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea quibusdam reiciendis ab ex voluptatum exercitationem cumque, dolores voluptate odio minima quod iure quam quis et eaque excepturi ipsam! Voluptates."
-          reactionsCount={5}
-        />
-        <Message
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea quibusdam reiciendis ab ex voluptatum exercitationem cumque, dolores voluptate odio minima quod iure quam quis et eaque excepturi ipsam! Voluptates."
-          reactionsCount={6}
-          answered
-        />
-        <Message
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea quibusdam reiciendis ab ex voluptatum exercitationem cumque, dolores voluptate odio minima quod iure quam quis et eaque excepturi ipsam! Voluptates."
-          reactionsCount={1}
-        />
-        <Message
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea quibusdam reiciendis ab ex voluptatum exercitationem cumque, dolores voluptate odio minima quod iure quam quis et eaque excepturi ipsam! Voluptates."
-          reactionsCount={0}
-        />
-        <Message
-          text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur ea quibusdam reiciendis ab ex voluptatum exercitationem cumque, dolores voluptate odio minima quod iure quam quis et eaque excepturi ipsam! Voluptates."
-          reactionsCount={9}
-        />
-      </ol>
+      <Suspense fallback={<p>Carregando...</p>}>
+        <Messages />
+      </Suspense>
     </div>
   );
 }
